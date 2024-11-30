f35894dbe3e9706ee084968acec5b6db6cad24e3
Rishabh Garg <narishabh.garg@gmail.com>
2024-11-30 18:48:06 +0530

    Implemented Basic UI for Item Stats and its Setter Functions

--- Assets/Scripts/UI/UIService.cs
+++ Assets/Scripts/UI/UIService.cs
@@ -1,5 +1,7 @@
+using ServiceLocator.Item;
 using TMPro;
 using UnityEngine;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 namespace ServiceLocator.UI
@@ -11,6 +13,7 @@ namespace ServiceLocator.UI
         [SerializeField] private Transform inventoryMenuButtonPanel;
         [SerializeField] private Transform shopGrid; // Assign Shop Content Game Object inside Shop Panel
         [SerializeField] private Transform shopMenuButtonPanel;
+        [SerializeField] private Transform itemMenuButtonPanel;
 
         [Header("UI Elements")]
         [SerializeField] private TMP_Text inventoryEmptyText;
@@ -19,6 +22,13 @@ namespace ServiceLocator.UI
         [SerializeField] private TMP_Text shopEmptyText;
         [SerializeField] private TMP_Text shopItemTypeText;
         [SerializeField] private TMP_Text shopItemsCountText;
+        [SerializeField] private RawImage itemImage;
+        [SerializeField] private TMP_Text itemDescription;
+        [SerializeField] private TMP_Text itemPrice;
+        [SerializeField] private TMP_Text itemWeight;
+        [SerializeField] private TMP_Text itemType;
+        [SerializeField] private TMP_Text itemRarity;
+        [SerializeField] private TMP_Text itemQuantity;
 
         public void Init() { }
 
@@ -27,6 +37,7 @@ namespace ServiceLocator.UI
         public Transform GetInventoryButtonPanel() => inventoryMenuButtonPanel;
         public Transform GetShopGrid() => shopGrid;
         public Transform GetShopButtonPanel() => shopMenuButtonPanel;
+        public Transform GetItemButtonPanel() => itemMenuButtonPanel;
 
         // Setters
         public void UpdateInventoryEmptyText(bool _isEmpty)
@@ -65,6 +76,53 @@ namespace ServiceLocator.UI
             {
                 _button.interactable = _isInteractable;
             }
+        }
+        private void SetItemImage(Sprite _sprite)
+        {
+            itemImage.texture = _sprite.texture;
+        }
+        private void SetItemDescription(string _text)
+        {
+            itemDescription.text = _text;
+        }
+        private void SetItemPrice(int _value)
+        {
+            itemPrice.text = $"Price: {_value}";
+        }
+        private void SetItemWeight(float _value)
+        {
+            itemWeight.text = $"Weight: {_value}";
+        }
+        private void SetItemType(string _text)
+        {
+            itemType.text = $"Type: {_text}";
+        }
+        private void SetItemRarity(string _text)
+        {
+            itemRarity.text = $"Rarity: {_text}";
+        }
+        private void SetItemQuantity(int _value)
+        {
+            itemQuantity.text = $"Quantity: {_value}x";
+        }
+        public void SetItemText(ItemModel _itemModel)
+        {
+            SetItemImage(_itemModel.Icon);
+            SetItemDescription(_itemModel.Description);
+
+            if (_itemModel.UISection == UISection.Inventory)
+            {
+                SetItemPrice(_itemModel.SellingPrice);
+            }
+            else if (_itemModel.UISection == UISection.Shop)
+            {
+                SetItemPrice(_itemModel.BuyingPrice);
+            }
+
+            SetItemWeight(_itemModel.Weight);
+            SetItemType(_itemModel.Type.ToString());
+            SetItemRarity(_itemModel.Rarity.ToString());
+            SetItemQuantity(_itemModel.Quantity);
         }
     }
 }
